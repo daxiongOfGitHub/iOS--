6cce52a50fb3ec1ba6c27798f0cb76a3150ab900
first <first@mytest.cn>
2015-11-02 11:40:17 +0800

    新建美团工程

---
+++ 美团项目/美团项目/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  美团项目
+//
+//  Created by ma c on 15/11/2.
+//  Copyright (c) 2015年 ma c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 美团项目/美团项目/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  美团项目
+//
+//  Created by ma c on 15/11/2.
+//  Copyright (c) 2015年 ma c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
